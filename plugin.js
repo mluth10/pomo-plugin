346feada8f941f83ff508ee7f64c0b515a2d97d0
Sara Liszeski <sara.liszeski@duke.edu>
2020-12-05 14:31:01 -0500

    pretty

--- plugin.js
+++ plugin.js
@@ -1,7 +1,7 @@
 const pomodoroTimer = document.querySelector('#pomodoro-timer')
 const startButton = document.querySelector('#pomodoro-start')
 const pauseButton = document.querySelector('#pomodoro-pause')
-const stopButton = document.querySelector('#pomodoro=stop')
+const stopButton = document.querySelector('#pomodoro-stop')
 
 
 // START
